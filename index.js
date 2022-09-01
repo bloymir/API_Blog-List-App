6fe618a57618ed7501f227a6171a795a4980c0e7
Nelson Tapia <ntapiapinto@gmail.com>
2022-09-01 02:43:57 -0400

    Inicializando proyecto

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -13,31 +14,30 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGO_DB
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+app.get('/api/blogs', (req, res) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
+      res.json(blogs)
     })
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
+app.post('/api/blogs', (req, res) => {
+  const blog = new Blog(req.body)
   blog
     .save()
     .then(result => {
-      response.status(201).json(result)
+      res.status(201).json(result)
     })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
